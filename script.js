6043fac768e88e3fc2e0f3dfe17d1b38787cbf24
Aung04 <s10243228@connect.np.edu.sg>
2024-02-08 23:09:28 +0800

    final

--- script.js
+++ script.js
@@ -45,4 +45,91 @@ function resetActiveBtn(){
         btn.classList.remove('active-btn');
     });
 }
+const product =[
+    {
+        image: 'images/V200W_2.png',
+        title: 'Mikasa V200W',
+        price: 80
+    },
+    {
+        image: 'images/V300W_2__22302.webp',
+        title: 'Mikasa V300W',
+        price: 60
+    },
+    {
+        image: 'images/sky_elite_fresh_blue-removebg-preview.png',
+        title: 'ACSIS Sky Elite',
+        price: 179
+    },
+    {
+        image: 'images/tights-removebg-preview.png',
+        title: 'McDavid Tights',
+        price: 79
+    },
+]
 
+const categories = [...new Set(product.map((item)=>
+    {return item}))];
+
+    function delElement(a){
+        categories.splice(a, 1);
+        displaycart();
+    }
+
+    function promo(){
+        let promocode=document.getElementById('promocode').value;
+        if(promocode==1234){
+            displaycart(50);
+        }
+        else(
+            prompt("Enter correct promo code")
+        )
+    }
+
+function displaycart(c){
+    let j=0, total=0;
+    document.getElementById("itemA").innerHTML = categories.length + " Items";
+    document.getElementById("itemB").innerHTML = categories.length + " Items";
+    if(categories.length==0){
+        document.getElementById("root").innerHTML="Your cart is empty";
+
+        document.getElementById("totalA").innerHTML = "$ 00.00";
+        document.getElementById("totalB").innerHTML = "$ 00.00";
+    }
+    else{
+        document.getElementById("root").innerHTML = categories.map((items)=>{
+            let {image, title, price} = items;
+            total = total+price;
+            document.getElementById("totalA").innerHTML = "$ "+ total +".00";
+
+            if(c==50){
+                document.getElementById("totalB").innerHTML="$ "+(total-c)+".00";
+            }else{
+                document.getElementById("totalB").innerHTML="$ "+total+ ".00";
+            }
+
+            return(
+                `<tr>
+                    <td width="150"><div class="img-box"><img class="img" src=${image}></div></td>
+                    <td width="360"><p style='font-size:15px;'>${title}</p></td>
+                    <td width="150"><h2 style='font-size:15px; color:red; '>$ ${price}.00</h2></td>
+                    <td width="70">`+"<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></td>"+
+                `</tr>`
+            );
+        }).join('');
+    }
+}
+displaycart();
+
+let count=0;
+const counter=document.getElementById('counter'); 
+document.getElementById('btn_add').addEventListener('click',event => {
+    const cl=counter.classList;
+    const c='animated-counter';
+    count++;
+    counter.innerText=count;
+    cl.remove(c,cl.contains(c));
+    setTimeout(() =>
+    counter.classList.add('animated-counter'),1)
+
+});
